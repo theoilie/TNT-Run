c323daf3621b4fc13e9480bdeb1aaac29cd1c832
Lactem <lactem@outlook.com>
2014-07-24 11:27:05 -0700

    Message update

--- TNT Run/src/net/galaxygaming/tntrun/TNTRun.java
+++ TNT Run/src/net/galaxygaming/tntrun/TNTRun.java
@@ -5,24 +5,29 @@ import net.galaxygaming.dispenser.team.Spectator;
 import net.galaxygaming.selection.Selection;
 import net.galaxygaming.util.LocationUtil;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
 
 public class TNTRun extends GameBase {
 	private Location spawn;
 	private Selection arena;
 	private String winner;
-	private Spectator spectatorTeam = new Spectator();
+	private String joinMessage, remainingMessage;
+	private Spectator spectatorTeam;
 	
 	private void setSpawn(Location spawn) {
 		this.spawn = spawn;
 		getConfig().set("spawn", LocationUtil.serializeLocation(spawn));
-		saveConfig();
+		save();
 	}
 	
 	private void setArena(Selection arena) {
 		this.arena = arena;
 		getConfig().set("arena", arena);
+		save();
 	}
 
 	public Location getSpawn() {
@@ -55,8 +60,30 @@ public class TNTRun extends GameBase {
 	public void onLoad() {
 		spawn = LocationUtil.deserializeLocation(getConfig().getString("spawn"));
 		arena = (Selection) getConfig().get("arena");
+		
 		addComponent("arena");
 		addComponent("spawn");
+		
+		spectatorTeam = new Spectator();
+		board = Bukkit.getScoreboardManager().getNewScoreboard();
+		objective = board.registerNewObjective
+			(ChatColor.translateAlternateColorCodes('&', "&4TNT &cRun"), "dummy");
+		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+		objective.getScore(ChatColor.translateAlternateColorCodes('&', "&6&lPlayers")).setScore(2);
+		lastPlayerCount = getPlayers().length;
+		objective.getScore(lastPlayerCount + "").setScore(1);
+		
+		int length = getPlayers().length;
+		getConfig().addDefault("remaining message", 
+			"(&6" + (length - spectatorTeam.getSize()) 
+			+ "&r/&6" + length + "&r)");
+		getConfig().addDefault(
+				"join message", "(&6" + getPlayers().length
+					+ "&r/&6" + getConfig().getInt("maximum players")
+					+ "&r)");
+		save();
+		joinMessage = getConfig().getString("join message");
+		remainingMessage = getConfig().getString("remaining message");
 	}
 
 	@Override
@@ -68,7 +95,6 @@ public class TNTRun extends GameBase {
 
 	@Override
 	public void onTick() {
-		super.onTick();
 	}
 
 	@Override
@@ -94,7 +120,22 @@ public class TNTRun extends GameBase {
 	}
 	
 	public String getRemainingMessage() {
-		int length = getPlayers().length;
-		return "(&6" + (length - spectatorTeam.getSize()) + "&r/&6" + length + "&r) remaining.";
+		return remainingMessage;
+	}
+	
+	public String getJoinMessage() {
+		return joinMessage;
+	}
+
+	@Override
+	public void onPlayerJoin(Player player) {
+		broadcast("&6" + player.getDisplayName() 
+			+ "&rhas joined." + joinMessage);
+	}
+
+	@Override
+	public void onPlayerLeave(Player player) {
+		broadcast("&6" + player.getDisplayName() 
+			+ "&rhas joined." + joinMessage);
 	}
 }
